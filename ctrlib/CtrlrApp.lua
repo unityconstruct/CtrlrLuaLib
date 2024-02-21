c82c0bd816fb192447940862feb931498edf7cd7
uc <uc@unityconstruct.org>
2024-02-20 22:51:50 -0600

    implementing modules within CtrlrApp

--- ctrlib/CtrlrApp.lua
+++ ctrlib/CtrlrApp.lua
@@ -1,13 +1,66 @@
 #!/usr/bin/lua
 local ctrlrEnv = require 'ctrlib.CtrlrEnv'
-local sysexParser = require 'ctrlib.utils.SysexParser'
 local dataUtils = require 'ctrlib.utils.DataUtils'
+local SysexParser = require 'ctrlib.utils.SysexParser' -- instantiate via function call - LOCAL
+-- local sysexUtils = require 'ctrlib.utils.SysexParser' -- instantiate via function call - LOCAL
+
+print("CtrlrApp: HI! Ctrlr")
+
+
+-- syx = SysexParser {}
+-- . syntax
+-- sysexUtils.globalFunction()
+-- sysexUtils.localFunction()
+-- SysexParser.globalFunction()
+-- SysexParser.localFunction()
+-- : syntax
+-- sysexUtils:globalFunction()
+-- sysexUtils:localFunction()
+-- SysexParser:globalFunction()
+-- SysexParser:localFunction()
+
 
 local function test1()
     print("CtrlrApp:test1{}")
+    SysexParser.globalFunction()
 end
 
-print("CtrlrApp: HI! Ctrlr")
+test1()
+
+
+
+--[[ SetupDump_1C tests ]]--
+-- instantiate the utility object that: holds sysex Specs and incoming message dumps
+-- local sysexUtils = sysexParser:new()
+-- local sysexUtils = SysexParser.SysexDumps:new()
+local sysexUtils = SysexParser.SysexDumps
+--[[
+    ]]--
+-- update the util with a SetupDump message
+sysexUtils.SysexSetupDump_1C = SysexParser.parseSyxToTable(SysexParser.sysexMessageSetupDump)
+-- now search the same SetupDumpSpec_1C in the utility
+local result = SysexParser.fetchSysexParam2Byte(sysexUtils.SysexSetupDump_1C,sysexUtils.SysexSetupDumpSpec_1C.MIDI_A_CONTROL)
+-- local sortedTable = tableSort(sysexUtils.SetupDumpSpec_1C)
+
+
+-- fetchDumpToValueTable test that scans sysexdump using DumpSpec, then create valueTable for each Parameter Field
+-- local valueTable = fetchDumpToValueTable(sysexUtils.SysexSetupDump_1C, SysexSetupDumpSpec_1C)
+local valueTable = SysexParser.fetchDumpToValueTable(sysexUtils.SysexSetupDump_1C, sysexUtils.SysexSetupDumpSpec_1C)
+SysexParser.printValueTable(valueTable)
+
+
+
+--[[ DEBUG HALT ]]--
+
+local stop = "STOP!"
+
+
+
+
+
+
+
+
 
 return {
     test1 = test1
